f4abe6c2231daa8a86f02833288231cbb0e6abf6
Inigo Surguy <inigo.surguy@67bricks.com>
2022-02-15 20:44:39 +0000

    Don't join gang factions until ready to start gang

--- src/crime/startGang.ts
+++ src/crime/startGang.ts
@@ -4,16 +4,18 @@ export async function main(ns: NS): Promise<void> {
 	if (ns.gang.inGang()) {
 		return;
 	} else {
-		const gangFactions = [ "Tetrads", "Slum Snakes" ];
-		gangFactions.filter(g => !ns.getPlayer().factions.includes(g))
-					.forEach(g => ns.joinFaction(g));
+		if (ns.heart.break() < -54000) {
+			const gangFactions = [ "Tetrads", "Slum Snakes" ];
+			gangFactions.filter(g => !ns.getPlayer().factions.includes(g))
+						.forEach(g => ns.joinFaction(g));
 
-		const gangName = (ns.getPlayer().factions.includes("Tetrads")) ? "Tetrads" : "Slum Snakes";
-		const wasCreated = ns.gang.createGang(gangName);
-		if (wasCreated) {
-			ns.tprint("Started gang");
-			ns.run("/crime/manageGang.js");
-			ns.run("/crime/intermittentWarfare.js");
+			const gangName = (ns.getPlayer().factions.includes("Tetrads")) ? "Tetrads" : "Slum Snakes";
+			const wasCreated = ns.gang.createGang(gangName);
+			if (wasCreated) {
+				ns.tprint("Started gang");
+				ns.run("/crime/manageGang.js");
+				ns.run("/crime/intermittentWarfare.js");
+			}
 		}
 	}
 }
