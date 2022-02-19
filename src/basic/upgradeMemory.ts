2076e59527a79eee8f5965604eb3319439e92c6e
Inigo Surguy <inigo.surguy@67bricks.com>
2022-02-19 22:22:59 +0000

    Upgrading memory should restart attacks

--- src/basic/upgradeMemory.ts
+++ src/basic/upgradeMemory.ts
@@ -2,7 +2,12 @@ import { NS } from '@ns'
 
 export async function main(ns : NS) : Promise<void> {
 	try {
-		ns.upgradeHomeRam();
+		if (ns.upgradeHomeRam()) {
+			if (ns.getServerMaxRam("home")<=8192) {
+				// Kill running attacks, because we're likely to want to switch target at the beginning
+				ns.run("/attack/killAttacks.js");
+			}
+		}
 	} catch (error) {
 		ns.print("Cannot upgrade RAM - no Singularity functions");
 	}
