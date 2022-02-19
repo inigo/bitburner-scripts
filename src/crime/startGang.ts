1d2e20c0a80b1ae92da28204795f05f245a4e361
Inigo Surguy <inigo.surguy@67bricks.com>
2022-02-19 17:35:40 +0000

    Use Speakers as a gang faction too

--- src/crime/startGang.ts
+++ src/crime/startGang.ts
@@ -5,11 +5,11 @@ export async function main(ns: NS): Promise<void> {
 		return;
 	} else {
 		if (ns.heart.break() < -54000) {
-			const gangFactions = [ "Tetrads", "Slum Snakes" ];
+			const gangFactions = [ "Speakers for the Dead", "Tetrads", "Slum Snakes" ];
 			gangFactions.filter(g => !ns.getPlayer().factions.includes(g))
 						.forEach(g => ns.joinFaction(g));
 
-			const gangName = (ns.getPlayer().factions.includes("Tetrads")) ? "Tetrads" : "Slum Snakes";
+			const gangName = gangFactions.find(f => ns.getPlayer().factions.includes(f));
 			const wasCreated = ns.gang.createGang(gangName);
 			if (wasCreated) {
 				ns.tprint("Started gang");
