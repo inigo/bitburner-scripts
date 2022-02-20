a2eecd84925c46b4d520e378ad88e0d74b1f5874
Inigo Surguy <inigo.surguy@67bricks.com>
2022-02-20 21:00:17 +0000

    Make upgrade memory script sell shares if necessary

--- src/basic/upgradeMemory.ts
+++ src/basic/upgradeMemory.ts
@@ -1,14 +1,12 @@
 import { NS } from '@ns'
+import { buyWithShares } from "tix/libShareSelling"; 
 
 export async function main(ns : NS) : Promise<void> {
-	try {
-		if (ns.upgradeHomeRam()) {
-			if (ns.getServerMaxRam("home")<=8192) {
-				// Kill running attacks, because we're likely to want to switch target at the beginning
-				ns.run("/attack/killAttacks.js");
-			}
+	const upgradeSucceeded = await buyWithShares(ns, ns.getUpgradeHomeRamCost(), () => ns.upgradeHomeRam());
+	if (upgradeSucceeded) {
+		if (ns.getServerMaxRam("home")<=8192) {
+			// Kill running attacks, because we're likely to want to switch target at the beginning
+			ns.run("/attack/killAttacks.js");
 		}
-	} catch (error) {
-		ns.print("Cannot upgrade RAM - no Singularity functions");
 	}
 }
---
+++ src/tix/libShareSelling.ts
@@ -0,0 +1,20 @@
+import { NS } from '@ns'
+import { getOwnedShareValue, sellAllShares, reportShareStatus } from '/tix/libTix.js';
+
+/**
+ * Make a purchase, selling shares if necessary to finance it.
+ */
+export async function buyWithShares(ns: NS, cost: number, buyFn: () => boolean): Promise<boolean> {
+	const availableMoney = ns.getServerMoneyAvailable("home");
+	const allAvailableMoney = availableMoney + getOwnedShareValue(ns);
+
+	if (availableMoney>=cost) {
+		return buyFn();
+	} else if (availableMoney < cost && allAvailableMoney >= cost) {
+		sellAllShares(ns);
+        await reportShareStatus(ns);
+		return buyFn();
+	} else {
+		return false;
+	}
+}
