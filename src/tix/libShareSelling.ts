d2eb8e9b459f00d7ff03d504a43955454fa9e6ca
Inigo Surguy <inigo.surguy@67bricks.com>
2022-02-21 09:55:27 +0000

    Pause share trading after selling, so we don't repeatedly lose money on rapid buying and selling

--- src/tix/libShareSelling.ts
+++ src/tix/libShareSelling.ts
@@ -1,5 +1,5 @@
 import { NS } from '@ns'
-import { getOwnedShareValue, sellAllShares, reportShareStatus } from '/tix/libTix.js';
+import { getOwnedShareValue, sellAllShares, reportShareStatus, pauseTrading } from '/tix/libTix.js';
 
 /**
  * Make a purchase, selling shares if necessary to finance it.
@@ -11,6 +11,7 @@ export async function buyWithShares(ns: NS, cost: number, buyFn: () => boolean):
 	if (availableMoney>=cost) {
 		return buyFn();
 	} else if (availableMoney < cost && allAvailableMoney >= cost) {
+		await pauseTrading(ns, 60);
 		sellAllShares(ns);
         await reportShareStatus(ns);
 		return buyFn();
