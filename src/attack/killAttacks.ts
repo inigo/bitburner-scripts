7cdd3d1433e980fa31fdec71e07df95024d9b26a
Inigo Surguy <inigo.surguy@67bricks.com>
2024-12-09 22:06:36 +0000

    Fix incorrect path for killing scripts

--- src/attack/killAttacks.ts
+++ src/attack/killAttacks.ts
@@ -12,10 +12,10 @@ export async function main(ns : NS) : Promise<void> {
 export async function killAttacks(ns: NS, target = "all"): Promise<void> {
 	const host = ns.getHostname();
     
-	const killedControllers = killMatchingScripts(ns, host, ["/attack/attack.js"], target);
-	const killedHacks = killMatchingScripts(ns, host, ["/attack/hack.js"], target);
+	const killedControllers = killMatchingScripts(ns, host, ["attack/attack.js"], target);
+	const killedHacks = killMatchingScripts(ns, host, ["attack/hack.js"], target);
 	await ns.sleep(1000); // Reduces the chance that we'll leave the target in a non-ideal state
-	const killedAttacks = killMatchingScripts(ns, host, ["/attack/grow.js", "/attack/hack.js", "/attack/weaken.js"], target);
+	const killedAttacks = killMatchingScripts(ns, host, ["attack/grow.js", "attack/hack.js", "attack/weaken.js"], target);
 	
 	const totalKilled = killedControllers + killedAttacks + killedHacks;
 	ns.tprint(`Killed ${totalKilled} processes`);
