ad533ba0290fe8d420f1410a23341bc70bef11a9
Inigo Surguy <inigo.surguy@67bricks.com>
2024-12-14 21:02:06 +0000

    Add documentation

--- src/goTo.ts
+++ src/goTo.ts
@@ -1,11 +1,9 @@
 import { manuallyConnectTo } from "basic/installBackdoors"; 
 import { NS } from '@ns'
 
-/**
- * Switch terminal to the specified server
- */
+/// Switch terminal to the specified server
 export async function main(ns : NS) : Promise<void> {
-	const destination = ns.args[0] as string;
+    const destination = ns.args[0] as string;
     if (destination==null) {
         ns.tprint("Specify a server to connect to");
     } else {
@@ -16,4 +14,3 @@ export async function main(ns : NS) : Promise<void> {
 export function autocomplete(data : AutocompleteData) : string[] {
     return [...data.servers];
 }
-
