449e0406354da07ec7a5fc75aa1ce5b696a7650f
Inigo Surguy <inigo.surguy@gmail.com>
2024-12-24 12:52:47 +0000

    Support React TSX files again

---
+++ src/vendored/react.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+// @ts-ignore
+export default React;
+// @ts-ignore
+export type * from 'react';
+// @ts-ignore
+export const {
+  Children,
+  Fragment,
+  Component,
+  Profiler,
+  PureComponent,
+  cloneElement,
+  createContext,
+  createElement,
+  createFactory,
+  createRef,
+  forwardRef,
+  isValidElement,
+  lazy,
+  memo,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useEffect,
+  useImperativeHandle,
+  useLayoutEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  version,
+  Suspense,
+  StrictMode,
+  // @ts-ignore
+} = React;
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
             /** path to your source code */
             '@': resolve(__dirname, 'src'),
             '/src': resolve(__dirname, 'src'),
+            react: resolve(__dirname, 'src', 'vendored', 'react.js'),
         },
     },
     build: {
@@ -23,6 +24,14 @@ export default defineConfig({
                 transform: true,
             },
             {pattern: 'src/**/*.{script,txt}'},
-        ],
+            {
+                pattern: 'src/**/*.tsx',
+                transform: true,
+                location: (file) => ({filename: file.replace(/[jt]sx?$/, 'js').replace(/^src/, '')}),
+            }
+],
     },
+    esbuild: {
+        jsx: 'transform',
+    }
 });
